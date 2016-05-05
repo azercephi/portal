bc1d2bbb1f0c78b56111cbf65015bab205da3f42
Jessica_B_Li <eilica668@gmail.com>
2016-05-05 00:21:57 -0700

    Continued experimenting with tab api to be able to track user movement across tabs. All movement currently logged onto console. Not all movements are disjoint. Added file with notes about Chrome behavior.

--- background.js
+++ background.js
@@ -1,36 +1,18 @@
-/**
- * Identifies which tab user is currently viewing and
- * prints to console.log in background html */
+/* Tracks user behavior as they change tabs */
+/* Identifies when user:
+ *   - switches between existing tabs
+ *   - changes url/link within tab
+ *   - when tab is switched in from Google Instant
+ */
 
 /* Declare global variable that tracks which tab is currently in view 
  * Initialize to dummy value. */
-var viewingId = -1;
+var viewingId = undefined;
 
 // Initializes viewingID to first tab clicked by user
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   viewingId = tabs[0].id;
   console.log("query: Initial Tab " + viewingId);
-});
-
-// Detects when tab content has changed.
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
-  if (changeInfo.status == "complete" && tabId == viewingId) {
-    console.log(viewingId + " has been updated.");
-    /* This way works poorly
-    if (changeInfo.url != undefined) {
-	  console.log("        new URL: " + changeInfo.url);
-	  console.log("        new title: " + changeInfo.title);
-	  */
-	  /* works a bit better */
-	  // get and display info
-	  console.log("        new URL: " + updatedTab.url);
-	  console.log("        new title: " + updatedTab.title);
-  }
-
-	/* get tab information 
-    chrome.tabs.get(selectedId, function(tab) {
-      console.log(tab.url);
-    }); */
 });
 
 /* Detects when viewer has switched between tabs */
@@ -41,30 +23,55 @@ chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
   console.log("Switched tabs to " + viewingId);
   // get and display info
   chrome.tabs.get(viewingId, function(tab) {
-  	console.log("    " + tab.url);
-  	console.log("    " + tab.title);
+    console.log("    " + tab.url);
+    console.log("    " + tab.title);
   });
 });
 
+// Detects when tab content has changed.
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
+  if (changeInfo.status == "complete" && tabId == viewingId) {
+    console.log(viewingId + " has been updated.");
+    /* This way works poorly
+    if (changeInfo.url != undefined) {
+    console.log("        new URL: " + changeInfo.url);
+    console.log("        new title: " + changeInfo.title);
+    */
+    /* works a bit better */
+    // get and display info
+    console.log("        new URL: " + updatedTab.url);
+    console.log("        new title: " + updatedTab.title);
+  }
+
+  /* get tab information 
+    chrome.tabs.get(selectedId, function(tab) {
+      console.log(tab.url);
+    }); */
+});
+
+/* Fires when tab is closed */
+chrome.tabs.onRemoved.addListener( function (tabId, info) {
+  console.log("Tab " + tabId + " was removed");
+});
+
+/* Perhaps could use with tab closed
+chrome.tabs.onCreated.addListener(function (newTab) {
+  console.log("Created new tab: " + newTab.tabId);
+  console.log("    " + newTab.url);
+  console.log("    " + newTab.title);
+}); */
+
+/* Fires when tab is replaced by another tab that Google prepared tab
+ * beforehand by using Google Instant predictions */
+chrome.tabs.onReplaced.addListener( function (addedTabId, removedTabId) {
+  console.log("Tab " + addedTabId + " replaced " + removedTabId);
+});
+
+/* perhaps use chrome.tabs.getCurrent */
+
 /* History API **************************************************************/
 
-/*
-chrome.history.search(
-			{ 
-				'text' : '', 
-				'startTime' : start_time, 
-				'endTime' : end_time,
-				'maxResults' : 100
-			}, 
-			function (arrayHistItems) {
-				var counter = 0;
-				arrayHistItems.forEach(function (histItem) {
-					counter++;
-					if (counter <= 32) {
-						console.log("{ id : '" + histItem.id 
-							+  "' , url : '" + histItem.url.slice(0, 40) 
-							+ "...'} <p>");
-					}
-				});
-			});
-			*/
+/* top Sites API ************************************************************/
+chrome.topSites.get(function(mostVisited) {
+  mostVisited.forEach(function(site) { console.log(site); });
+});
